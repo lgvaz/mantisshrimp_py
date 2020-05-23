cf7f565262ecee7bdd89aa94b89901a90901fcfc
lgvaz <lucasgouvaz@gmail.com>
2020-05-23 16:16:54 -0300

    implements learner

---
+++ mantisshrimp/visualize/show_results.py
@@ -0,0 +1,6 @@
+from ..imports import *
+from .show_preds import *
+
+def show_results(learn, k=5):
+    rs = random.choices(learn.valid_dl.dataset.records, k=k)
+    show_preds(*learn.m.predict(rs=rs))
